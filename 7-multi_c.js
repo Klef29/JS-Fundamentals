0ececea278989380210c9273f47d4e677c1b6256
Klef29 <albertasare212@gmail.com>
2025-05-24 18:38:00 +0000

    Update on '-3' user input

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
-const arg = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 
-if (isNaN(arg) || arg < 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
-  for (let i = 0; i < arg; i++) {
+  for (let i = 0; i < x; i++) {
     console.log("C is fun");
   }
 }
