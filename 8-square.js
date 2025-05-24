1c3c77d0b56192025bc9cf6525724db50cff109c
Klef29 <albertasare212@gmail.com>
2025-05-24 19:01:22 +0000

    update Squares

--- 8-square.js
+++ 8-square.js
@@ -1,10 +1,9 @@
-const x = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2]);
 
-if (isNaN(x)){
-    console.log('Missing size');
-} 
-    else{
-for(let i = 0; i < x; i++){
-   console.log("X".repeat(x))
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
 }
- }
